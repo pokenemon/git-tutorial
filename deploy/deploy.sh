791ca584565a99142c139bbd9009c9c80723e93a
kenji kawabata <a633b3c1@yahoo.co.jp>
2017-04-09 23:38:47 +0900

    kkk

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,22 @@
-echo "test"
+#!/bin/bash
+
+set -ux
+
+clf_template_name="clf_template.json"
+
+export AWS_ACCESS_KEY_ID=`echo $DEV_AWS_ACCESS_KEY_ID`
+export AWS_SECRET_ACCESS_KEY=`echo $DEV_AWS_SECRET_ACCESS_KEY`
+
+echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
+
+echo "## create new ec2 instance"
+clf_stack_name="$deploy-test-`date +%s`"
+template_dir=$(cd $(dirname $0) $$ pwd)
+
+aws cloudformation create-stack --stack-name "clf_stack_name" --template-body file://"$template_dir"/dev/"$clf_template_name"
+
+if [ $? != 0 ]; then
+ exit 1
+fi
+
+echo "wait until create-stack"
