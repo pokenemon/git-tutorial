2dbb446cf21fd2db7103718c4817b78b472d08d4
kenji kawabata <a633b3c1@yahoo.co.jp>
2017-04-24 00:25:36 +0900

    iiii

--- deploy/after_success.sh
+++ deploy/after_success.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 sudo pip install awscli;
 sudo apt-get install jq;
 mkdir -p /vat/tmp/release;
 tar -zcf /var/tmp/release/kawabata.tar.gz --exclude ".git" ./;
-if [[ "$TRAVIS_BRANCH" =~ ^dev.*$ ]]; then
+if [[ "$TRAVIS_BRANCH" =~ ^dev.*$ ]] ; then
   DEV_DEPLOY_BRANCH=$TRAVIS_BRANCH
 fi
+#if [[ "$TRAVIS_BRANCH" =~ ^dev.*$ ]]; then
+#  DEV_DEPLOY_BRANCH=$TRAVIS_BRANCH
+#fi
 echo $DEV_DEPLOY_BRANCH
